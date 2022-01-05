13742bd63aeb01eed7bcd4b586d0b1dcc76de1d7
Jason Libertelli <jason.libertelli@gmail.com>
2022-01-05 17:20:54 -0500

    first commit/ setup-models

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+let sequelize;
+
+// jawsDB
+
+module.exports = sequelize;
---
+++ models/User.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model{
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            
+        //set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        //set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData){
+             updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+             return updatedUserData;
+        }
+      },
+      // pass in sequelize connection
+      sequelize,
+      //dont automatically create createdAt/updatedAt timestamp fields 
+      timestamps: false,
+      // don't pluralize name of databse table
+      freezeTableName: true,
+      // use underscores instead of camel-casing
+      underscored: true,
+      // make it so our model name stays lowercase in the database 
+      modelName: 'user'
+    } 
+);
+
+module.exports = User;
