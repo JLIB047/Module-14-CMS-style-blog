68493f3faa924dce301cb7f0984cf1918f52e60e
Jason Libertelli <jason.libertelli@gmail.com>
2022-01-06 17:35:49 -0500

    api- routes

--- config/connection.js
+++ config/connection.js
@@ -3,5 +3,10 @@ require('dotenv').config();
 let sequelize;
 
 // jawsDB
+sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
 
 module.exports = sequelize;
